e2e5f1cbbc21c85a61c97fc761d97406fb3fa400
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-11-07 19:20:57 +0800

    Initial commit

---
+++ torch_template/__init__.py
@@ -0,0 +1,6 @@
+__version__ = 0.01
+__author__ = 'Kevin, Wei'
+__lib_name__ = 'torch_template'
+__email__ = 'kevin@yxtechs.cn'
+__description__ = 'This is a front for pytorch lib in deep learning'
+__url__ = ''
