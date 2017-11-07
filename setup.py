5d0a35d169c7564c2831e7ce129569af29535f00
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-11-07 19:25:13 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,49 +5,11 @@ maintainer_email = 'kevin@yxtechs.cn'
 author = maintainer
 author_email = maintainer_email
 description = "'This is a front for pytorch lib in deep learning"
-verson = '0.01'
+version = '0.01'
 name = 'torch template'
 
 long_description = """
-=====
-VASPy
-=====
-.. image:: https://travis-ci.org/PytLab/VASPy.svg?branch=master
-    :target: https://travis-ci.org/PytLab/VASPy
-    :alt: Build Status
-.. image:: https://landscape.io/github/PytLab/VASPy/master/landscape.svg?style=flat
-    :target: https://landscape.io/github/PytLab/VASPy/master
-    :alt: Code Health
-.. image:: https://codecov.io/gh/PytLab/VASPy/branch/master/graph/badge.svg
-    :target: https://codecov.io/gh/PytLab/VASPy
-.. image:: https://img.shields.io/badge/python-3.5, 2.7-green.svg
-    :target: https://www.python.org/downloads/release/python-351/
-    :alt: platform
-.. image:: https://img.shields.io/badge/pypi-v0.8.6-blue.svg
-    :target: https://pypi.python.org/pypi/vaspy/
-    :alt: versions
-Introduction
-------------
-VASPy is a pure Python library designed to make it easy and quick to manipulate VASP files.
-You can use VASPy to manipulate VASP files in command lins or write your own python scripts to process VASP files and visualize VASP data.
-In `/scripts <https://github.com/PytLab/VASPy/tree/master/scripts>`_ , there are some scripts written by me for daily use.
-Installation
-------------
-1. Via pip(recommend)::
-    pip install vaspy
-2. Via easy_install::
-    easy_install vaspy
-3. From source::
-    python setup.py install
-If you want to use **mayavi** to visualize VASP data, it is recommened to install `Canopy environment <https://store.enthought.com/downloads/#default>`_ on your device instead of installing it manually.
-After installing canopy, you can set corresponding aliases, for example:
-.. code-block:: shell
-    alias canopy='/Users/<yourname>/Library/Enthought/Canopy/edm/envs/User/bin/python'
-    alias canopy-pip='/Users/zjshao/Library/Enthought/Canopy/edm/envs/User/bin/pip'
-    alias canopy-ipython='/Users/<yourname>/Library/Enthought/Canopy/edm/envs/User/bin/ipython'
-    alias canopy-jupyter='/Users/<yourname>/Library/Enthought/Canopy/edm/envs/User/bin/jupyter'
-Then you can install VASPy to canopy::
-    canopy-pip install vaspy
+Tobe added from readme
 """
 
 install_requires = [
@@ -60,7 +22,7 @@ packages = [
     'torch_template',
 ]
 platforms = ['linux', 'macOS']
-url = 'https://github.com/zhenyuanwei/VASPy'
+url = 'https://github.com/zhenyuanwei/torch_template'
 download_url = ''
 classifiers = [
     'Development Status :: 3 - Alpha',
@@ -85,5 +47,4 @@ setup(author=author,
       url=url,
       download_url=download_url,
       version=version,
-#      test_suite="tests",
       classifiers=classifiers)
